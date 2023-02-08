5fd1d847cf096d81cc0f26c8adea70d69dd935f5
Victor Olsson <victor.olsson@consultant.volvo.com>
2023-02-08 09:18:17 +0100

    add polybar multiple screens

--- polybar/.config/polybar/polybar.sh
+++ polybar/.config/polybar/polybar.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env sh
-
+#
 # Terminate already running bar instances
 killall -q polybar
-
-# Wait until the processes have been shut down
+#
+## Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Launch polybar
-nm-applet &
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+#
+## Launch polybar
+#nm-applet &
+#polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+#
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
