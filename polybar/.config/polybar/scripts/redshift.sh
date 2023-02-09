85273aa7d5dc42ec1397c8dccc3d1432c2237e92
Victor Olsson <victor.olsson@consultant.volvo.com>
2023-02-09 22:29:33 +0100

    add scroll for redshift

--- polybar/.config/polybar/scripts/redshift.sh
+++ polybar/.config/polybar/scripts/redshift.sh
@@ -1,11 +1,20 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 envFile=~/.config/polybar/scripts/env.sh
+changeValue=300
 
 changeMode() {
   sed -i "s/REDSHIFT=$1/REDSHIFT=$2/g" $envFile 
   REDSHIFT=$2
   echo $REDSHIFT
+}
+
+changeTemp() {
+  if [ "$2" -gt 1000 ] && [ "$2" -lt 25000 ]
+  then
+    sed -i "s/REDSHIFT_TEMP=$1/REDSHIFT_TEMP=$2/g" $envFile 
+    redshift -P -O $((REDSHIFT_TEMP+changeValue))
+  fi
 }
 
 case $1 in 
@@ -18,6 +27,12 @@ case $1 in
       changeMode "$REDSHIFT" on
       redshift -O "$REDSHIFT_TEMP"
     fi
+    ;;
+  increase)
+    changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP+changeValue))
+    ;;
+  decrease)
+    changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP-changeValue));
     ;;
   temperature)
     case $REDSHIFT in
